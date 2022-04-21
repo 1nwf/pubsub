031043683f1461421ec4105723862cf8b4992064
nwf03 <nwf.aloufi@gmail.com>
2022-04-20 21:46:37 -0400

    added client struct

--- client/src/lib.rs
+++ client/src/lib.rs
@@ -1,3 +1,22 @@
+mod message;
+use std::net::TcpStream;
+
+use message::Message;
+
+pub struct Client {
+    conn: TcpStream,
+}
+impl Client {
+    pub fn new(addr: &str) -> Self {
+        let conn = TcpStream::connect(addr).unwrap();
+        Self { conn }
+    }
+    pub fn subscribe(channel: String) {}
+    pub fn unsubscribe(channel: String) {}
+
+    pub fn publish(channel: String, data: String) {}
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
