aff20a8d3a0bd3655ccd3666b19c4e87f06e3dba
nwf03 <nwf.aloufi@gmail.com>
2022-04-24 02:42:09 -0400

    changed client mutable references to immutable

--- client/src/incoming.rs
+++ client/src/incoming.rs
@@ -1,16 +1,17 @@
-use std::io::Read;
+use std::io::{BufReader, Read};
 
 use crate::Client;
 
 pub struct Incoming<'a> {
-    pub client: &'a mut Client,
+    pub client: &'a Client,
 }
 
 impl<'a> Iterator for Incoming<'a> {
     type Item = String;
     fn next(&mut self) -> Option<Self::Item> {
         let mut buf = [0; 128];
-        let n = self.client.conn.read(&mut buf).unwrap();
+        let mut reader = BufReader::new(&self.client.conn);
+        let n = reader.read(&mut buf).unwrap();
         let str_data = String::from_utf8(buf[..n].into()).unwrap();
         Some(str_data)
     }
--- client/src/lib.rs
+++ client/src/lib.rs
@@ -1,6 +1,9 @@
 mod incoming;
 mod message;
-use std::{io::Write, net::TcpStream};
+use std::{
+    io::{BufWriter, Write},
+    net::TcpStream,
+};
 
 use incoming::Incoming;
 use message::Message;
@@ -13,26 +16,29 @@ impl Client {
         let conn = TcpStream::connect(addr).unwrap();
         Self { conn }
     }
-    pub fn subscribe(&mut self, channel: &String) {
+    pub fn subscribe(&self, channel: &String) {
         let msg = Message::subscribe(channel);
         let msg_str = ron::to_string(&msg).unwrap();
-        let _ = self.conn.write_all(msg_str.as_bytes());
-        self.conn.flush().unwrap();
+        let mut writer = BufWriter::new(&self.conn);
+        let _ = writer.write_all(msg_str.as_bytes());
+        writer.flush().unwrap();
     }
-    pub fn unsubscribe(&mut self, channel: &String) {
+    pub fn unsubscribe(&self, channel: &String) {
         let msg = Message::unsubscribe(channel);
         let msg_str = ron::to_string(&msg).unwrap();
-        let _ = self.conn.write_all(msg_str.as_bytes());
-        self.conn.flush().unwrap();
+        let mut writer = BufWriter::new(&self.conn);
+        let _ = writer.write_all(msg_str.as_bytes());
+        writer.flush().unwrap();
     }
 
-    pub fn publish(&mut self, channel: &String, data: &String) {
+    pub fn publish(&self, channel: &String, data: &String) {
         let msg = Message::publish(channel, data);
         let msg_str = ron::to_string(&msg).unwrap();
-        let _ = self.conn.write_all(msg_str.as_bytes());
-        self.conn.flush().unwrap();
+        let mut writer = BufWriter::new(&self.conn);
+        let _ = writer.write_all(msg_str.as_bytes());
+        writer.flush().unwrap();
     }
-    pub fn recieve_message(&mut self) -> Incoming {
+    pub fn recieve_message(&self) -> Incoming {
         Incoming { client: self }
     }
 }
