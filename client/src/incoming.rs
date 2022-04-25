17962cce3ad309b0beef1d3bfef533f5158b921c
nwf03 <nwf.aloufi@gmail.com>
2022-04-25 02:47:22 -0400

    removed leading and trailing quotation marks from recieved data

--- client/src/incoming.rs
+++ client/src/incoming.rs
@@ -13,6 +13,9 @@ impl<'a> Iterator for Incoming<'a> {
         let mut reader = BufReader::new(&self.client.conn);
         let n = reader.read(&mut buf).unwrap();
         let str_data = String::from_utf8(buf[..n].into()).unwrap();
-        Some(str_data)
+        let mut chars = str_data.chars();
+        chars.next();
+        chars.next_back();
+        Some(String::from(chars.as_str()))
     }
 }
